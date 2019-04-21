388cc0333cdbde6e8eda2e8c98aba2fd12d559ac
Caambodia <36169027+Caambodia@users.noreply.github.com>
2019-04-21 14:29:44 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -42,7 +42,7 @@ volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
 volume_4 = Conv2D(64, (1,1), padding='same', activation='relu')(volume_4)
 
 # Concatenate all volumes of the Inception module
-inception_module_1 = keras.layers.concatenate([volume_1, volume_2, volume_3, volume_4], axis = 3)
+# inception_module_1 = keras.layers.concatenate([volume_1, volume_2, volume_3, volume_4], axis = 3)
 
 
 # Create Volumes for the Inception module 2
@@ -58,10 +58,10 @@ volume_d = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
 volume_d = Conv2D(64, (1,1), padding='same', activation='relu')(volume_d)
 
 # Concatenate all volumes of the Inception module
-inception_module_2 = keras.layers.concatenate([volume_a, volume_b, volume_c, volume_d], axis = 3)
+inception_module = keras.layers.concatenate([volume_a, volume_b, volume_c, volume_d, volume_1, volume_2, volume_3, volume_4], axis = 3)
 
 
-inception_module = keras.layers.concatenate(inception_module_1, inception_module_2, axis = 3)
+# inception_module = keras.layers.concatenate(inception_module_1, inception_module_2, axis = 3)
 
 output = Flatten()(inception_module)
 out    = Dense(10, activation='softmax')(output)
