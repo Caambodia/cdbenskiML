ab184ec68d7634ee4624d869e67580a47938c5ba
Caambodia <36169027+Caambodia@users.noreply.github.com>
2019-04-21 22:59:51 -0400

    Update Q4_Test.py

--- Q4_Test.py
+++ Q4_Test.py
@@ -11,8 +11,8 @@ from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 
 seed_text = 'you will probably be prepared to admit that you are not exceptional'
-n_chars_to_predict = 10
-seq_length = 50
+n_chars_to_predict = 50
+seq_length = 10
 
 # load the model and mapping
 model = load_model('model.h5')
